8bd4b5c2cb4c5946b6ebd31b8340725fe3475ad6
sangtenuk <tenukians@gmail.com>
2022-10-09 03:47:20 +0800

    kick starter

--- src/app/components/pickup-call-card/pickup-call-card.component.ts
+++ src/app/components/pickup-call-card/pickup-call-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pickup-call-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pickup-call-card.component.scss'],
 })
 export class PickupCallCardComponent implements OnInit {
+
+  @Input() hasHeader: boolean;
+  @Input() hasFooter: boolean;
+
+  @Input() status: string;
+  @Input() updateAt: string;
+  @Input() createdAt: string;
+  @Input() notes: string;
 
   constructor() { }
 
